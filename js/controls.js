bdcef82b5effc678f9b997573beaab6a6511159c
Justen <justenbarget@gmail.com>
2015-12-30 15:42:56 +0000

    adding initial files

---
+++ js/controls.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+      $('.testimonials').slick({
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHOver: false
+      });
+    });
